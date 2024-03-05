460ad188d43a0dcb70bbdd06b21298345132dce0
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-03-05 14:01:48 +0700

    Finish stateless now date and time

---
+++ android/app/src/main/kotlin/com/example/format_date/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.format_date
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Date Format"),
+        ),
+        body: Center(
+          child: Text(
+            DateFormat.yMMMEd().add_Hms().format(DateTime.now()),
+            style: TextStyle(fontSize: 25),
+          ),
+        ),
+      ),
+    );
+  }
+}
